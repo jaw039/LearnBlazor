23adac228d47f4cbe01ec90ff1bd29ad20f278c8
Jackie Wang <jaw039@ucsd.edu>
2025-05-24 16:34:54 -0700

    added two way bind value

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+
+        public int Price { get; set; }
+    }
+}
